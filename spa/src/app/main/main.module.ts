d6fc7d85f6fe7f675a64c4d87b3cedff1f2ea0c7
tania <tani.zhelezniak@gmail.com>
2019-09-17 15:59:02 +0300

    sloder

--- spa/src/app/main/main.module.ts
+++ spa/src/app/main/main.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { MainSliderComponent } from './main-slider/main-slider.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainSliderComponent, PageComponent],
   imports: [
     CommonModule,
     MainRoutingModule
